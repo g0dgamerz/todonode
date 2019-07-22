703ef38eee15e20dabd7038c6b0c42ecdc6e0cd9
Jidesh <jideshvaidya@gmail.com>
2019-07-22 18:07:41 +0545

    updated

--- index.js
+++ index.js
@@ -1,48 +1,80 @@
-//dependencies required for the app
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
-
+var csvWriter = require('csv-write-stream')
+const csv = require('csv-parser');
+const fs = require('fs');
+const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+var task = [];
+fs.readFile(__dirname + '/tasks.csv' , 'utf8', (err,data) => {
+    if(err) {
+
+    }
+    if(data){
+        task = data.split('\n');
+        console.log(complete)
+    }
+})
+// var task2=[];
+
 app.set("view engine", "ejs");
-//render css files
-app.use(express.static("public"));
 
-//placeholders for added task
-var task = ["buy socks", "practise with nodejs"];
-//placeholders for removed task
-var complete = ["finish jquery"];
+var complete = [];
+fs.readFile(__dirname + '/completed.csv' , 'utf8', (err,data) => {
+    if(err) {
 
-//post route for adding new task 
-app.post("/addtask", function(req, res) {
-    var newTask = req.body.newtask;
-    //add the new task from the post route
-    task.push(newTask);
+    }
+    if(data){
+        complete = data.split('\n');
+        console.log(complete)
+    }
+})
+
+
+app.post("/addtask", urlencodedParser, function(req, res) {
+    var taskNew = req.body.newtask;
+    
+    
+    // res.render('display',taskNew);
+    task.push(taskNew);
+
+    fs.appendFile('tasks.csv',`\n${taskNew}`,(err)=>console.error(err));
+    // fs.readFile('tasks.csv','utf-8',(err,data)=>{
+    //     if(err) throw err;
+    //     if(data){
+    //         console.log('reading');
+    //         console.log(data);
+    //         task.push(data);
+    //     }
+    // })
     res.redirect("/");
 });
 
 app.post("/removetask", function(req, res) {
     var completeTask = req.body.check;
-    //check for the "typeof" the different completed task, then add into the complete task
     if (typeof completeTask === "string") {
         complete.push(completeTask);
-        //check if the completed task already exits in the task when checked, then remove it
+        fs.appendFile('completed.csv',`${completeTask}\r`,(err)=>console.error(err));
         task.splice(task.indexOf(completeTask), 1);
-    } else if (typeof completeTask === "object") {
-        for (var i = 0; i < completeTask.length; i++) {
-            complete.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1);
-        }
-    }
+    } 
+    // else if (typeof completeTask === "object") {
+    //     for (var i = 0; i < completeTask.length; i++) {
+    //         complete.push(completeTask[i]);
+    //         task.splice(task.indexOf(completeTask[i]), 1);
+    //     }
+    // }
     res.redirect("/");
 });
 
-//render the ejs and display added task, completed task
 app.get("/", function(req, res) {
     res.render("index", { task: task, complete: complete });
 });
 
-//set app to listen on port 3000
-app.listen(3000, function() {
-    console.log("server is running on port 3000");
+app.listen(2000, function() {
+    console.log("server is running on port 2000");
 });
+
+app.use(express.static("public"));
